d4d1577bd340c90b25d48c7c3af0bdab14797aca
Karl Sabo <karl.sabo@gmail.com>
2025-12-12 12:37:24 -0700

    Extend `isIssueOrBug` to include "change request" as a valid issue type.

--- utilities/src/commonMain/kotlin/com/github/karlsabo/jira/Issue.kt
+++ utilities/src/commonMain/kotlin/com/github/karlsabo/jira/Issue.kt
@@ -300,7 +300,7 @@ fun Issue.isMilestone(): Boolean {
 fun Issue.isIssueOrBug(): Boolean {
     if (fields.issueType?.name == null) return false
     return when (fields.issueType.name.lowercase()) {
-        "bug", "issue", "story", "subtask", "artifact", "task", "vulnerability", "request", "design story", "ds story" -> true
+        "bug", "issue", "story", "subtask", "artifact", "task", "vulnerability", "request", "design story", "ds story", "change request" -> true
         "epic", "theme", "parent artifact", "r&d initiative", "sub-task", "company initiative" -> false
         else -> {
             val message = "Unhandled issue type `${fields.issueType.name}`, Info: $key, ${fields.summary}"
